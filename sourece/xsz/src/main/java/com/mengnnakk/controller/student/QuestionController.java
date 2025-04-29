5ae8297d6b4636abea3afa461c9cf4aa93e7d5df
mengnankkkk <mengnankkzhou@gmail.com>
2025-04-29 18:57:07 +0800

    UploadController完成

---
+++ sourece/xsz/src/main/java/com/mengnnakk/controller/student/QuestionController.java
@@ -0,0 +1,19 @@
+package com.mengnnakk.controller.student;
+
+import com.mengnnakk.base.BaseApiController;
+import com.mengnnakk.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("StudentQuestionController")
+@RequestMapping(value = "/api/student/question")
+public class QuestionController extends BaseApiController {
+
+    private final QuestionService questionService;
+
+    @Autowired
+    public QuestionController(QuestionService questionService) {
+        this.questionService = questionService;
+    }
+}
